f616c0fb9fc498bd4edb6230ca684d52154079a6
Marilia Pereira <Marilia.Pereira-ext@ab-Inbev.com>
2023-05-03 16:31:20 -0300

    ✨ feat: init slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,54 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { useKeenSlider } from "keen-slider/react"
+import 'keen-slider/keen-slider.min.css'
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor : "$primary"
-})
+import shirt1 from '../../public/assets/shirt/shirt1.png'
+import shirt2 from '../../public/assets/shirt/shirt2.png'
+import shirt3 from '../../public/assets/shirt/shirt3.png'
+
 
 export default function Home() {
-  return (
-    <>
-      <h1>Wello World</h1>
-      <Button>Teste</Button>
-    </>
-  )
-}
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+    }
+  })
+  return ( 
+  <HomeContainer ref={sliderRef} className="keen-slider">
+    <Product className="keen-slider__slide">
+      <Image src={shirt1} alt="shirt01" width={520} height={480}/>
+
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+
+    <Product className="keen-slider__slide">
+      <Image src={shirt2} alt="shirt01" width={520} height={480}/>
+
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+
+    <Product className="keen-slider__slide">
+      <Image src={shirt3} alt="shirt01" width={520} height={480}/>
+
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+    <Product className="keen-slider__slide">
+      <Image src={shirt3} alt="shirt01" width={520} height={480}/>
+
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+  </HomeContainer>
+)}
