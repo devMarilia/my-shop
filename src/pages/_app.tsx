3d07c46db8bbc1c96943fce7696f1f19188bbeec
Marilia Pereira <Marilia.Pereira-ext@ab-Inbev.com>
2023-05-02 21:25:04 -0300

    ✨ feat: Includ image and styled homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import { globalStyles } from '../styles/global';
 import type { AppProps } from 'next/app'
+import Image from 'next/image';
+import logoImage from '../../public/assets/logo.svg'
+import { globalStyles } from '../styles/global';
+import { Contatiner, Header } from '../styles/pages/app';
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Contatiner>
+      <Header>
+        <Image src={logoImage} alt="logo"/>
+      </Header>
+      <Component {...pageProps} />
+    </Contatiner>
+  )
 }
